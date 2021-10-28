cfefc54eed48aea19c3fc11f733d407411465770
İsmail Kılıç <152120171049@ogrenci.ogu.edu.tr>
2021-10-28 17:43:14 +0300

    deleted files

--- pages/Login/test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const test = () => {
-  return <div>asdsasadsa 12321321231</div>;
-};
-
-export default test;
--- pages/test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const test = () => {
-  return <div>asdasdasd</div>;
-};
-
-export default test;
