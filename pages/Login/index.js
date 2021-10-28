5cc57ce2b67636bb1eed61791fd5a9214a3141b1
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-10-28 17:36:27 +0300

    Update home page

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const aasd = () => {
   return (
     <div>
-      welcome to index js ozkan repo
+      welcome to index js ismail repo
       <div>21321321</div>
     </div>
   );
