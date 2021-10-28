d3db8e496aae66f0a2ecf35d9a77b12de78d6b5a
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-10-28 16:50:58 +0300

    Build home page

---
+++ pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, nulla!
+      </p>
+      <button>asdasd</button>
+    </div>
+  );
+};
+
+export default index;
