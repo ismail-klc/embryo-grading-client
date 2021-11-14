a84d63fb321a75c8037ffe57bf7729f6dfbfd2ac
Ismail Kılıç <ismail000728@gmail.com>
2021-11-14 21:00:28 +0300

    patients table added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../public/css/image-upload.css'
 import '../public/css/globals.css'
+import '../public/css/table.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/patients/index.js
+++ pages/patients/index.js
@@ -1,10 +1,72 @@
 import React from 'react'
 import Admin from '../../components/Layouts/Admin'
+import DataTable from 'react-data-table-component';
+
+const columns = [
+    {
+        name: 'Id',
+        selector: row => row.id,
+    },
+    {
+        name: 'TC No',
+        selector: row => row.tcNo,
+    },
+    {
+        name: 'Ad Soyad',
+        selector: row => row.fullName,
+        sortable: true
+    },
+    {
+        name: '',
+        selector: row => <button className="py-2 px-3 text-white rounded-md bg-gray-600">Detay</button>,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        tcNo: '12345678901',
+        fullName: 'Hasta 1',
+    },
+    {
+        id: 2,
+        tcNo: '12345678902',
+        fullName: 'Hasta 2',
+    },
+    {
+        id: 3,
+        tcNo: '12345678903',
+        fullName: 'Hasta 3',
+    },
+    {
+        id: 4,
+        tcNo: '12345678904',
+        fullName: 'Hasta 4',
+    },
+    {
+        id: 5,
+        tcNo: '12345678905',
+        fullName: 'Hasta 5',
+    },
+    {
+        id: 6,
+        tcNo: '12345678906',
+        fullName: 'Hasta 6',
+    },
+]
 
 const PatientsPage = () => {
+
     return (
         <Admin>
-            patients
+            <DataTable
+                title="Hastalar"
+                columns={columns}
+                data={data}
+                pagination
+                responsive
+                striped
+            />
         </Admin>
     )
 }
