d2ec0d6c197a166f3ec36148e92b5dfb081c46c8
Ismail Kılıç <ismail000728@gmail.com>
2021-12-11 23:31:02 +0300

    conf change

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,18 +1,21 @@
 import { NextResponse } from 'next/server'
 
 async function authMiddleware(req, ev) {
-    // const data = await fetch(`${process.env.API}/doctors/me`,
-    //     {
-    //         credentials: true, headers: req.headers
-    //     })
-    // const authUrls = ["/login", "/register"]
-    // if (data.status === 200 && authUrls.includes(req.url)) {
-    //     return NextResponse.redirect('/')
-    // }
+    // const token = req.cookies.jwt;
 
-    // if (data.status === 401 && !authUrls.includes(req.url)) {
-    //     return NextResponse.redirect('/login');
-    // }
+    const authUrls = ["/login", "/register"]
+
+    const data = await fetch(`${process.env.API}/doctors/me`,
+        {
+            credentials: true, headers: req.headers
+        })
+    if (data.status === 200 && authUrls.includes(req.url)) {
+        return NextResponse.redirect('/')
+    }
+
+    if (data.status === 401 && !authUrls.includes(req.url)) {
+        return NextResponse.redirect('/login');
+    }
 
     return NextResponse.next()
 }
